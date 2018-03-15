b77014baee916a234566d2767cddc31b57c71cfd
rpyoungdale <youngdale95@gmail.com>
2018-03-15 11:59:19 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+require "pry"
+
+class Song
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      if !artist_hash[artist]
+        artist_hash[artist] = 1
+      elsif artist_hash[artist]
+        artist_hash[artist]+=1
+      end
+    end
+    artist_hash
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count+=1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.genre_count
+    genres_num = {}
+    @@genres.each do |genre|
+      if !genres_num[genre]
+        genres_num[genre] = 1
+      elsif genres_num[genre]
+        genres_num[genre]+=1
+      end
+    end
+    genres_num
+  end
+end
